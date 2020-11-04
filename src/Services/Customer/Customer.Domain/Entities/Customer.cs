b9602f288d4d8623e54cb7b890630c987278cb75
S9034B <chewchoong.chen@seb.se>
2020-11-04 10:59:31 +0800

    Added Customer.Domain and Customer.Data

---
+++ src/Services/Customer/Customer.Domain/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Customers.Domain.Entities
+{
+    public partial class Customer
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime? Birthday { get; set; }
+
+        public int? Age { get; set; }
+    }
+}
