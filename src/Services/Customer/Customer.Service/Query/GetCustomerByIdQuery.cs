2d7a056d9948960d3adaa284b35d9f2ebfedca94
ChewChoong <ccchen1213@gmail.com>
2020-11-05 17:30:56 +0800

    added customer.service

---
+++ src/Services/Customer/Customer.Service/Query/GetCustomerByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MediatR;
+
+namespace Customer.Service.Query
+{
+    public class GetCustomerByIdQuery : IRequest<Domain.Entities.Customer>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/Services/Customer/Customer.Service/Query/GetCustomerByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Customer.Data.Repository;
+using MediatR;
+
+namespace Customer.Service.Query
+{
+    public class GetCustomerByIdQueryHandler : IRequestHandler<GetCustomerByIdQuery, Domain.Entities.Customer>
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public GetCustomerByIdQueryHandler(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<Domain.Entities.Customer> Handle(GetCustomerByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _customerRepository.GetCustomerByIdAsync(request.Id, cancellationToken);
+        }
+    }
+}
